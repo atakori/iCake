9d3c9479e0bb8a3e203afc9bc9f154074fb83490
Alfredo <atakori@outlook.com>
2018-03-04 23:38:23 -0500

    added n2 solution for stocks.js

--- stocks.js
+++ stocks.js
@@ -3,17 +3,20 @@ const stockPrices= [10,7,5,8,11,22,55]
 
 function getMaxProfit(stockPrices) {
     // calculate the max profit
+
+    //O(n2) solution
     let maxProfit;
     for(let i=0; i<stockPrices.length;i++) {
   
-        for(let j=i+1;j<stockPrices.length-1; j++) {
-            if(stockPrices[i] < stockPrices[j]) {
-                let difference= stockPrices[j] - stockPrices[i];
-                if(maxProfit === undefined || difference > maxProfit) {
-                    maxProfit = difference;
-                }
+        for(let j=i+1;j<stockPrices.length; j++) {
+            let difference= stockPrices[j] - stockPrices[i];
+            if(maxProfit === undefined || difference > maxProfit) {
+            	maxProfit = difference;   
             }
         }
     }
     return maxProfit;
 }
+
+const stockPrices = [10, 7, 5, 4, 3,22];
+console.log(getMaxProfit(stockPrices));
