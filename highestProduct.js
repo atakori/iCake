678b45fbb6ea3ed9e6a7243ab50e33c5952bd0c0
Alfredo <atakori@outlook.com>
2018-03-07 00:48:39 -0500

    added solution for highestProduct.js

--- highestProduct.js
+++ highestProduct.js
@@ -1,5 +1,45 @@
+//find the highest product you can get from three Ints
+
+
+//could do three nesting loops, but O(n3)...
 function findHighestProduct(arr) {
+	if(arr.length <3) {
+		throw new Error('fxn requires an array with at least 3 values');
+	}
+	//declare values to keep track throuh looping
+	let highestProductOf3= arr[0]* arr[1] * arr[2];
+	let highestProductOf2= arr[0]* arr[1];
+	let lowestProductOf2= arr[0]* arr[1];
+	let highest= Math.max(arr[0], arr[1]);
+	let lowest= Math.min(arr[0], arr[1]);
 	
+	//starting at third item since first two are declared in vars
+	for(let i=2; i<arr.length; i++) {
+		let currentNum= arr[i];
+
+		//redeclare / update each variable
+		highestProductOf3= Math.max( highestProductOf3,
+			currentNum * highestProductOf2,
+			currentNum * lowestProductOf2
+			//this is needed to test two high - nums
+			);
+
+		highestProductOf2= Math.max(highestProductOf2,
+			currentNum * highest,
+			currentNum * lowest //two negative nums
+			);
+
+		lowestProductOf2= Math.min(lowestProductOf2,
+			currentNum * highest,
+			currentNum * lowest
+			);
+
+		highest= Math.max(currentNum, highest);
+		lowest= Math.min(currentNum, lowest);
+	}
+	return highestProductOf3;
 }
+
+//Note: Sorting method doesn't work with negative nums
 
 module.exports= findHighestProduct;
--- highestProduct.test.js
+++ highestProduct.test.js
@@ -4,4 +4,5 @@ test('should return the highest product from the three highest Ints', () => {
 	expect(findHighestProduct([1,2,3])).toBe(6);
 	expect(findHighestProduct([2,4,9,3,8])).toBe(288);
 	expect(findHighestProduct([-1,8,4,-3,-2])).toBe(48);
+	//test negative numbers
 })
