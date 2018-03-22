37fc065743fcade9a38181018886e215491cd5cb
Alfredo <atakori@outlook.com>
2018-03-21 23:33:57 -0400

    added memoized solution

--- countCoins.js
+++ countCoins.js
@@ -1,3 +1,52 @@
+//updating answer to include memoization
+
+/*function Change() {
+  //container to keep used nums
+  this.memo= {};
+}
+
+Change.prototype.makingChange= function(amountLeft, denominations, currentIndex) {
+    currentIndex= (typeof currentIndex !== 'undefined')? currentIndex : 0;
+
+    //check memo and return value if we got a solution previously
+    const memoKey= [amountLeft, currentIndex].join(', ');
+    if(this.memo.hasOwnProperty(memoKey)) {
+      return this.memo.memoKey;
+    }
+
+    //set base cases
+
+    if(amountLeft === 0) {
+      return 1;
+    }
+
+    //current combination of denoms do not work
+    if(amountLeft< 0) {
+      return 0;
+    }
+
+    // no more denominations to go through!
+    if(currentIndex === denominations.length) {
+      return 0;
+    }
+
+    const currentCoin= denominations[currentIndex];
+    let numPossibilities= 0;
+
+    //recursive loop
+    while(numPossibilities >=0) {
+      numPossibilities+= this.makingChange(amountLeft,denominations, currentIndex +1);
+      amountLeft-= currentCoin;
+    }
+
+    //save the answer in the memo so we can bypass it later
+    this.memo[memoKey]= numPossibilities;
+    return numPossibilities;    
+
+}
+
+module.exports= Change.makingChange;*/
+
 function makingChange(amountLeft, denominations, currentIndex) {
 	currentIndex= (typeof currentIndex !== 'undefined') ? currentIndex : 0;
 
